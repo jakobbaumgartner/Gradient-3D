77fa2383e50542019adbb816f9d31af104760a12
Jakob Baumgartner <jakob.baumgartner@hotmail.com>
2023-04-03 15:50:39 +0200

    added robot display, changed colors of display, change display scale

--- Code/Simulation/demo_room.m
+++ Code/Simulation/demo_room.m
@@ -8,57 +8,57 @@ space_width = 500;
 space_length = 500;
 
 % create grid obstacle
-grid = create_3d_grid(space_resolution, space_height, space_width, space_length);
+grid_occupancy = create_3d_grid(space_resolution, space_height, space_width, space_length);
 
 % add square table
-grid = add_box(grid, space_resolution, 200, 210, 100, 200, 1, 50);
-grid = add_box(grid, space_resolution, 290, 300, 100, 200, 1, 50);
-grid = add_box(grid, space_resolution, 200, 300, 100, 110, 1, 50);
-grid = add_box(grid, space_resolution, 200, 300, 190, 200, 1, 50);
+grid_occupancy = add_box(grid_occupancy, space_resolution, 200, 210, 100, 200, 1, 50);
+grid_occupancy = add_box(grid_occupancy, space_resolution, 290, 300, 100, 200, 1, 50);
+grid_occupancy = add_box(grid_occupancy, space_resolution, 200, 300, 100, 110, 1, 50);
+grid_occupancy = add_box(grid_occupancy, space_resolution, 200, 300, 190, 200, 1, 50);
 
 
 % add counter 1
-grid = add_box(grid, space_resolution, 90, 100, 100, 400, 1, 50);
-grid = add_box(grid, space_resolution, 1, 100, 100, 110, 1, 50);
+grid_occupancy = add_box(grid_occupancy, space_resolution, 90, 100, 100, 400, 1, 50);
+grid_occupancy = add_box(grid_occupancy, space_resolution, 1, 100, 100, 110, 1, 50);
 
 
 % add counter 2
-grid = add_box(grid, space_resolution, 100, 400, 300, 310, 1, 50);
-grid = add_box(grid, space_resolution, 390, 400, 300, 400, 1, 50);
+grid_occupancy = add_box(grid_occupancy, space_resolution, 100, 400, 300, 310, 1, 50);
+grid_occupancy = add_box(grid_occupancy, space_resolution, 390, 400, 300, 400, 1, 50);
 
 % -------------------------------------------------------------------------------------------
-
-
 
 %% ONLY VISUALIZATION
 % -------------------------------------------------------------------------------------------
 min_voxel_display_value = 0.05; % min value to draw voxel, to avoid rendering of empty voxels
 view_angle = [25, 65]; % angle at which we look at plot
-display_grid(grid, min_voxel_display_value)
+display_grid(grid_occupancy, min_voxel_display_value, space_resolution)
+
+hold on
 
 % display obstacles (with big patches for faster rendering)
-voxel([210 110 0]/space_resolution,[70 70 50]/space_resolution,'g',0.5)
-voxel([0 110 0]/space_resolution,[80 290 50]/space_resolution,'g',0.5)
-voxel([100 310 0]/space_resolution,[280 90 50]/space_resolution,'g',0.5)
+voxel([210 110 0],[70 70 50],'g',0.5)
+voxel([0 110 0],[80 290 50],'g',0.5)
+voxel([100 310 0],[280 90 50],'g',0.5)
 
 %     % add floor
 
         % Define the x and y ranges
-        x = linspace(0, 1, size(grid,1))*size(grid,1);
-        y = linspace(0, 1, size(grid,2))*size(grid,2);
+        x = linspace(0, 1, size(grid_occupancy,1)) * size(grid_occupancy,1) * space_resolution;
+        y = linspace(0, 1, size(grid_occupancy,2)) * size(grid_occupancy,2) * space_resolution;
         
         % Create a grid of points from x and y
         [X,Y] = meshgrid(x,y);
         
         % Define the function to plot
-        Z = zeros(size(grid,1),size(grid,2));
+        Z = zeros(size(grid_occupancy,1),size(grid_occupancy,2));
             
         % Plot the surface
-        surf(X,Y,Z, 'FaceAlpha',0.5, 'FaceColor','y');
+        surf(X,Y,Z, 'FaceAlpha',0.5, 'FaceColor',"#EDB120");
 
 
 % set fixed axis size
-axis([0 size(grid,1) 0 size(grid,2) 0 size(grid,3)])
+axis([0 size(grid_occupancy,1)*space_resolution 0 size(grid_occupancy,2)*space_resolution 0 size(grid_occupancy,3)*space_resolution])
 
 % set viewing angle
 view(view_angle);
@@ -69,9 +69,9 @@ view(view_angle);
 % -------------------------------------------------------------------------------------------
 
 % Create three sets of points
-points1 = [(130:0.6:160)' (80:1.6:160)' ones(51,1)*80];
-points2 = [(180:5.4:310)' (230:1.21:260)' ones(25,1)*80];
-points3 = [(315:5:450)' ones(28,1)*260 ones(28,1)*100];
+points1 = [(130:0.6:160)' (80:1.6:160)' ones(51,1)*100];
+points2 = [(180:5.4:310)' (230:1.21:260)' ones(25,1)*110];
+points3 = [(315:5:450)' ones(28,1)*260 ones(28,1)*120];
 
 % Combine the three sets of points into a single matrix
 points = [points1 ; points2; points3];
@@ -80,7 +80,7 @@ points = [points1 ; points2; points3];
 [X, Y, Z] = polyfit_xyz_trajectory(points);
 
 % Plot the polynomial curve in 3D space
-plot3(X/space_resolution, Y/space_resolution, Z/space_resolution, 'r-', 'LineWidth', 2);
+plot3(X, Y, Z, 'r-', 'LineWidth', 2);
 xlabel('X');
 ylabel('Y');
 zlabel('Z');
@@ -90,11 +90,13 @@ axis equal;
 
 %  -------------------------------------------------------------------------------------------
 
+
 %% OPTIMIZATION
 % -------------------------------------------------------------------------------------------
 
-
-
+robot_angles = [1.50 0 pi/2 0 pi/4 0 -pi/3 0 1.8675 0];
+[Abase, A01, A12, A23, A34, A45, A56, A67, T] = GeometricRobot(robot_angles);
+showRobot(Abase, A01, A12, A23, A34, A45, A56, A67,space_resolution*10, "#A2142F")
 
 
 
--- Code/Simulation/display_grid.m
+++ Code/Simulation/display_grid.m
@@ -1,4 +1,4 @@
-function display_grid(grid, min_display_value)
+function display_grid(grid, min_display_value, space_resolution)
     % This function displays a 3D space matrix as a 3D plot, where each voxel 
     % is represented by a cube. Only voxels with a value above the 
     % min_display_value threshold are displayed.
@@ -25,7 +25,7 @@ function display_grid(grid, min_display_value)
                     % draw a cube at the position defined by start, with 
                     % dimensions [1 1 1] and color black ('k'). The value 
                     % of the voxel is used to set the color of the cube.
-                    voxel(start, [1 1 1], 'k', voxel_value);
+                    voxel(start * space_resolution, [1 1 1] * space_resolution, 'k', voxel_value);
                 end
                 
                 % keep the same plot for subsequent voxel cubes
