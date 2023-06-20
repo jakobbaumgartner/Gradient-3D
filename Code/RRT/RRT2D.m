e0d7dc870329a0dc4ba8372d5269eec763364bed
JB <jakob.baumgartner@hotmail.com>
2023-06-20 17:58:22 +0200

    working RRT

--- Code/RRT/RRT2D.m
+++ Code/RRT/RRT2D.m
@@ -4,8 +4,8 @@ tic()
 
 % parameters
 fixed_distance = true; 
-distance_step = 2; % define a fixed distance step
-goal_distance = 2;
+distance_step = 1.5; % define a fixed distance step
+goal_distance = 1;
 max_branches = 10000; % max number of branches
 
 % grid dimensions
@@ -44,24 +44,26 @@ while true
 
     end
 
+
     % Check that new node is on grid
-    if (new_node(1) > 1 && new_node(2) > 1 && new_node(1) <= size(grid,1) && new_node(2) <= size(grid,2))
+    if (new_node(1) > 2 && new_node(2) > 2 && new_node(1) <= size(grid,1)-1 && new_node(2) <= size(grid,2)-1)
 
 %         new_node
         
         % Check that new branch doesn't intersect with obstacles
-        intersects = checkObstacleIntersection(grid, tree(index, 2:3), new_node);
+%         obstacleCheck = isObstacleFree(grid, tree(index,2), tree(index,3), new_node(1), new_node(2));
+        obstacleCheck = sum(sum(grid(ceil(new_node(1))-1:ceil(new_node(1))+1, ceil(new_node(2))-1:ceil(new_node(2))+1))) > 0;
 
     else
 
-        intersects = true;
+        obstacleCheck = true;
 
     end
        
 
 
     % Save new node to existing tree
-    if (~intersects)
+    if (~obstacleCheck)
 
         % Save node
         tree = [tree ; index new_node];
@@ -104,9 +106,54 @@ end
 
 end
 
-function obstacleDetected  = checkObstacleIntersection(grid, start_node, end_node)
-
+function obstacleCheck = isObstacleFree(obstacleGrid, x0, y0, x1, y1)
+    dx = abs(x1-x0);
+    dy = abs(y1-y0);
     
+    if x0 < x1
+        sx = 1;
+    else
+        sx = -1;
+    end
 
+    if y0 < y1
+        sy = 1;
+    else
+        sy = -1;
+    end
 
+    err = dx - dy;
+
+    while true
+        
+        % Check if current point is on grid
+          if x0 < 1 || y0 < 1 || x0 > size(obstacleGrid,1) || y0 > size(obstacleGrid,2) 
+            obstacleCheck = false;
+            return;
+          end
+
+        % Check if current point is an obstacle
+        if obstacleGrid(ceil(x0), ceil(y0)) == 1
+            obstacleCheck = false;
+            return;
+        end
+
+        if x0 == x1 && y0 == y1
+            break;
+        end
+
+        e2 = 2 * err;
+        
+        if e2 > -dy
+            err = err - dy;
+            x0 = x0 + sx;
+        end
+
+        if e2 < dx
+            err = err + dx;
+            y0 = y0 + sy;
+        end
+    end
+    
+    obstacleCheck = true;
 end
