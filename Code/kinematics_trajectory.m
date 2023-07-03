5b22f36498067968e45cdaf9855181f2ccf10242
JB <jakob.baumgartner@hotmail.com>
2023-07-03 13:47:10 +0200

    working manipulator - no obstacles

--- Code/kinematics_trajectory.m
+++ Code/kinematics_trajectory.m
@@ -14,7 +14,7 @@ ee_point = robot_transforms(1:3,4,8);
 % calculate distance from goal
 current_dist = norm(ee_point'- goal_point(1:3));
 
-% add starting positions
+% add starting positions to saved array
 EE_positions = [ee_point];
 joints_positions = [q];
 goal_distances = [current_dist];
@@ -22,26 +22,30 @@ goal_distances = [current_dist];
 % trajectory calculation loop
 while current_dist > goal_dist
 
-    % get panda manipulator Jacobian
-    [~,~,J]=kinmodel_panda(q); 
-    [~,robot] = GeometricPandaMATLAB(q, Tbase);
+    % OPTION JACOBIAN
+    [~,~,J]=kinmodel_panda(q); % calculate jacobian
 
-    % set configuration
-    config = homeConfiguration(robot);
-    
-    % set joint positions
-    for i = 1:1:7
-        config(i).JointPosition = q(i);
-    end
-
-    J2 = geometricJacobian(robot,config,'body7');
-    J3 = [J2(4:6,:) ; J2(1:3,:)];
-
-    J = J2;
-
+    % --------------------------------------------------
+    % OPTION MATLAB ROBOT TOOLBOX JACOBIAN
+%         [~,robot] = GeometricPandaMATLAB(q, Tbase); % get robot configuration
+%         config = homeConfiguration(robot); % set configuration
+%         
+%         % set joint positions
+%         for i = 1:1:7
+%             config(i).JointPosition = q(i);
+%         end
+%     
+%         % toolbox jacobian returns first three rows angular velocity and
+%         % second three rows linear velocities, change rows to be in line with
+%         % used convention in default jacobian
+%         J2 = geometricJacobian(robot,config,'body7');
+%         J3 = [J2(4:6,:) ; J2(1:3,:)];
+%     
+%         J = J3;
+    % --------------------------------------------------
 
     % calculate pseudo inverse
-    pinv_J = J'*(J*J'+ damping_factor^2 * eye(6))^-1;
+    pinv_J = J'*(J*J'+ damping_factor^2 * eye(6))^-1; % damping to avoid singularities
 
     % calculate ee velocities
     ee_vel = goal_point(1:3)' - ee_point;
