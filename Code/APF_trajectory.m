62e348978144be6ffdaa742181a3184aa96f8899
JB <jakob.baumgartner@hotmail.com>
2023-07-07 17:55:23 +0200

    APF_trajectory framework plan

---
+++ Code/APF_trajectory.m
@@ -0,0 +1,33 @@
+function [outputArg1,outputArg2] = APF_trajectory(inputArg1,inputArg2)
+
+    % calculate positions in 3D space for a number of points on a robot frame using geometric transformations
+
+    % get APF value in every of calculated 3D points
+    
+    % if A - closest point only
+
+        % find in which point APF is the highest
+
+        % calculate Jacobian in that point
+
+        % if there was a previously different point with highest APF calculate Jacobian in this previous point
+
+        % calculate weighting coefficients 
+
+        % calculate avoidance joints velocities
+
+    
+    % if B - all points on the robot
+
+        % calculate APF in every point
+
+        % calculate Jacobian in every point
+
+        % calculate weighting coefficients
+
+        % calculate avoidance joints velocities
+
+
+
+
+end
