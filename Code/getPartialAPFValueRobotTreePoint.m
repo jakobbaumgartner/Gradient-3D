ae9c40bb09d0562bef252dff24fa161c44d2c792
JB <jakob.baumgartner@hotmail.com>
2023-07-10 20:32:03 +0200

    comments

--- Code/getPartialAPFValueRobotTreePoint.m
+++ Code/getPartialAPFValueRobotTreePoint.m
@@ -1,5 +1,27 @@
 function [value, transform] = getPartialAPFValueRobotTreePoint(APF, resolution, robot_tree, Tbase, joints)
 
+    % Function: getPartialAPFValueRobotTreePoint
+    %
+    % Description:
+    %   This function calculates the value and transform for a given point in the
+    %   robot tree using the attractive potential field (APF) method. It takes the
+    %   APF, resolution, robot tree, base transformation, and joint values as inputs.
+    %
+    % Input:
+    %   - APF: The attractive potential field.
+    %   - resolution: The resolution for interpolating APF values.
+    %   - robot_tree: The robot tree structure.
+    %   - Tbase: The base transformation.
+    %   - joints: The joint values for the robot tree.
+    %
+    % Output:
+    %   - value: The interpolated APF value at the given point.
+    %   - transform: The transform for the given point in the robot tree.
+    %
+    % Usage:
+    %   [value, transform] = getPartialAPFValueRobotTreePoint(APF, resolution,
+    
+    
     % set configuration to a robot tree
     config = homeConfiguration(robot_tree); % Create an empty struct
     
