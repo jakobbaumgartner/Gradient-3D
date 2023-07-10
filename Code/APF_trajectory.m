6f76000218d5b598ea085a1e97e44caf7af8fe04
JB <jakob.baumgartner@hotmail.com>
2023-07-10 19:05:53 +0200

    partial segments transformations

--- Code/APF_trajectory.m
+++ Code/APF_trajectory.m
@@ -1,9 +1,9 @@
-function [outputArg1,outputArg2] = APF_trajectory(inputArg1,inputArg2)
+function transformations_list = APF_trajectory()
 
     % parameters
     % ---------------------------------------------------------
     num_of_segments = 7;
-    points_per_segment = [1 1 1 1 1 1 1];
+    points_per_segment = 5*[1 1 1 1 1 1 1];
    
     % this is a classic (Spong?) DH convention parameters table, on official Franka
     % Emika site there is a different (Craig) convention (that is a pain in the
@@ -19,7 +19,7 @@ function [outputArg1,outputArg2] = APF_trajectory(inputArg1,inputArg2)
     % ---------------------------------------------------------
 
     % list of transformations
-    transformations_list = {};
+    transformations_list = [];
 
     % current tree
     robot_tree = rigidBodyTree;
@@ -32,7 +32,7 @@ function [outputArg1,outputArg2] = APF_trajectory(inputArg1,inputArg2)
     for i = 1:1:num_of_segments % for every segment
 
         % for a number of points on segments
-%         for j = 1:1:points_per_segment(i)
+        for j = 1:1:points_per_segment(i)
 
             % new attaching joint
             new_joint = rigidBodyJoint('jnt'+string(i),'revolute');
@@ -41,22 +41,37 @@ function [outputArg1,outputArg2] = APF_trajectory(inputArg1,inputArg2)
             new_segment = rigidBody('body'+string(i));
     
             % set parameters
-            segment_param = dhparams(i,:);          % change this to get more segments
+            segment_param = [dhparams(i,1) * j / points_per_segment(i) , dhparams(i,2) , dhparams(i,3) * j / points_per_segment(i) , dhparams(i,4)] ;          % change this to get more segments
     
             % set transformation from last segment
             setFixedTransform(new_joint, segment_param,'dh');
     
             % attach joint to body
             new_segment.Joint = new_joint;
-    
-            % add body to the tree
-            addBody(robot_tree,new_segment,previous_body);
+
+            
+            if j > 1 % if this is not the first partial segment replace old segment
+                replaceBody(robot_tree, previous_body, new_segment)
+                
+            else % if it is first partial form of segment, add new segment
+                % add body to the tree
+                addBody(robot_tree, new_segment, previous_body);
+                
+
+            end
 
             % save current fixing body name
-            previous_body = 'body'+string(i)
+            previous_body = 'body' + string(i)
+
+            % save configuration
+            save_conf = {};
+            save_conf.tree = copy(robot_tree);
+            save_conf.segm_num = i;
+            save_conf.segm_part = j;
+            transformations_list = [transformations_list save_conf ];
 
 
-%         end
+        end
     end
 
     % get APF value in every of calculated 3D points
