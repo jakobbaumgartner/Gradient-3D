45b0d6c4274da99148d310a3bf44741acbeb82c9
JB <jakob.baumgartner@hotmail.com>
2023-07-11 14:23:28 +0200

    comments

--- Code/attractive_field_3D.m
+++ Code/attractive_field_3D.m
@@ -1,5 +1,26 @@
 function [grid_attractive] = attractive_field_3D(grid, goal_position, scale, varargin)
 
+    % Function: attractive_field_3D
+    %
+    % Description:
+    % This function calculates the attractive field in a 3D grid based on a
+    % goal position. The attractive field represents the strength of
+    % attraction towards the goal at each point in the grid.
+    %
+    % Input:
+    % - grid: 3D grid matrix
+    % - goal_position: 3-element vector representing the goal position
+    % - scale: scaling factor for the grid coordinates
+    % - (optional) Name-Value pairs:
+    % - K: scaling factor for the attractive field (default: 1)
+    %
+    % Output:
+    % - grid_attractive: 3D grid matrix representing the attractive field
+    %
+    % Usage:
+    % grid_attractive = attractive_field_3D(grid, goal_position, scale)
+    % grid_attractive = attractive_field_3D(grid, goal_position, scale, 'K', 2)
+
     % Create an input parser
     parser = inputParser;
     parser.CaseSensitive = false;
