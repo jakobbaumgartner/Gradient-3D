faddf868ba4333cc69be30f1fd8ca14336737855
JB <jakob.baumgartner@hotmail.com>
2023-07-17 21:29:10 +0200

    3D x-directional kernel

--- Code/directional_kernel_3d.m
+++ Code/directional_kernel_3d.m
@@ -1,49 +1,70 @@
-function [kernel] = directional_kernel_3d(direction, kernel_size, kernel_sigma, kernel_num_layers, kernel_layers_sigma, kernel_type)
-
-    % check inputs
-    % =========================================================================
-
-    % check that number of layers is odd
-    if mod(kernel_num_layers, 2) == 0
-        error('Number of layers must be odd');
-    end
-
-    % check that kernel size is odd
-    if mod(kernel_size, 2) == 0
-        error('Kernel size must be odd');
-    end
-
-    % check that direction is: 'x', 'y' or 'z'
-    if ~strcmp(direction, 'x') && ~strcmp(direction, 'y') && ~strcmp(direction, 'z')
-        error('Direction must be: ''x'', ''y'' or ''z''');
-    end
-
-    % check that kernel type is: 'gaussian', 'linear' or 'box'
-    if ~strcmp(kernel_type, 'gaussian') && ~strcmp(kernel_type, 'linear') && ~strcmp(kernel_type, 'box')
-        error('Kernel type must be: ''gaussian'', ''linear'' or ''box''');
-    end
-
-    % check that kernel sigma is a scalar
-    if ~isscalar(kernel_sigma)
-        error('Kernel sigma must be a scalar');
-    end
-
-    % check that kernel layers sigma is a scalar
-    if ~isscalar(kernel_layers_sigma)
-        error('Kernel layers sigma must be a scalar');
-    end
-
+function [kernel3D] = directional_kernel_3d(direction, kernel_length, kernel_sigma, kernel_width, kernel_width_sigma, kernel_height, kernel_height_sigma, kernel_type)
 
     % create kernel
     % =========================================================================
+
+    center = floor(kernel_length/2); % mid of the kernel
     
     % x kernel
     if strcmp(direction, 'x')
-        kernel = zeros(kernel_size, kernel_size, kernel_num_layers);
-        for i = 1:kernel_num_layers
-            kernel(:, :, i) = directional_kernel_2d('x', kernel_size, kernel_sigma, kernel_type);
+        if strcmp('linear',kernel_type)
+    
+            % prepare directional weights
+            kernel = [1:1:center 0 -flip(1:1:center)] / (center+1); 
+    
+            % extend weights to kernel width
+            center_width = floor(kernel_width/2);
+            width_extender = [1:1:center_width center_width+1 flip(1:1:center_width)]/(center_width+1);
+    
+            % generate 2D kernel
+            kernel2D = width_extender' * kernel;
+    
+            % extend weight to kernel height
+            center_height = floor(kernel_height/2);
+            height_extender = [1:1:center_height center_height+1 flip(1:1:center_height)]/(center_height+1);
+            
+            % create each layer
+            kernel3D = repmat(kernel2D, 1, 1, length(height_extender)) .* reshape(height_extender, 1, 1, []);
+
+    
+        elseif strcmp('gaussian',kernel_type)
+    
+    
+            % define the standard deviation (sigma)
+            sigma = 1;
+            
+            % generate gaussian weights
+            kernel = exp(-(1:center).^2/(2*sigma^2)); 
+            
+            % generate symmetric kernel
+            kernel = [flip(kernel) 1 kernel] / sum([flip(kernel) 1 kernel]);
+            
+            % prepare gaussian weights for width
+            center_width = floor(kernel_width/2);
+            width_extender = exp(-(1:center_width).^2/(2*sigma^2)); 
+            width_extender = [flip(width_extender) 1 width_extender] / sum([flip(width_extender) 1 width_extender]);
+            
+            % generate 2D kernel
+            kernel2D = width_extender' * kernel;
+            
+            % prepare gaussian weights for height
+            center_height = floor(kernel_height/2);
+            height_extender = exp(-(1:center_height).^2/(2*sigma^2));
+            height_extender = [flip(height_extender) 1 height_extender] / sum([flip(height_extender) 1 height_extender]);
+            
+            % initiate kernel matrix
+            kernel3D = zeros(kernel_width, kernel_length, kernel_height);
+            
+            % create each layer
+            kernel3D = repmat(kernel2D, 1, 1, length(height_extender)) .* reshape(height_extender, 1, 1, []);
+
+
+
         end
-    end
+
+
+     end
+
 
     % y kernel
 
@@ -55,10 +76,15 @@ function [kernel] = directional_kernel_3d(direction, kernel_size, kernel_sigma, 
 
 
 
+
+    end
+
+
+
+
     
 
 
 
 
 
-end
