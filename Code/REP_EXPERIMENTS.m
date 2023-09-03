d4dd4b4e0e21161c482b67bc34ad7bf2b328a22f
JB <jakob.baumgartner@hotmail.com>
2023-09-03 19:05:09 +0200

    function REP_field_display

---
+++ Code/REP_EXPERIMENTS.m
@@ -0,0 +1,40 @@
+% REPULSIVE FIELD GENERATION
+
+%% SET POINT
+point = [1,1,1]
+
+%% CREATE GRID
+% -----------------------------------------------------------
+
+% create grid
+grid = OctoGrid(2,2,2,100);
+
+% add the wall
+grid.addBox(0.2,0.8,0,1.6,0.1,1.5)
+
+%% GET REPULSIVE KERNEL
+% -----------------------------------------------------------
+
+% set directional kernels
+sigma = 11;
+kernel_length = 61;
+kernel_width = 9;
+kernel_height = kernel_width;
+
+% form kernels
+kernel_x = directional_kernel_3d('x', kernel_length, sigma, kernel_width, sigma, kernel_height, sigma, 'gaussian')/1000;
+kernel_y = directional_kernel_3d('y', kernel_length, sigma, kernel_width, sigma, kernel_height, sigma, 'gaussian')/1000;
+kernel_z = directional_kernel_3d('z', kernel_length, sigma, kernel_width, sigma, kernel_height, sigma, 'gaussian')/1000;
+
+%% GET REP FIELD VALUES
+% -----------------------------------------------------------
+
+[rep_values] = REP_field_calculation(grid, {kernel_x, kernel_y, kernel_z}, point)
+
+% generate vectors from values
+rep_vectors = eye(3) .* rep_values';
+
+%% DISPLAY REP VECTORS
+% -----------------------------------------------------------
+
+REP_field_display(grid, point, rep_vectors)
--- Code/REP_field_display.m
+++ Code/REP_field_display.m
@@ -1,8 +1,24 @@
-function [outputArg1,outputArg2] = REP_field_display(grid, vectors_list)
+function REP_field_display(grid, point, vectors_list)
+
+    figure()
+
+    % scaling factor
+    scale = 100;
+    
+    % display grid
+    grid.showGridVol3D(grid.grid,'floor',true,'height',false);
+
+    hold on
 
 
+    % display vectors
+    for i = 1:length(vectors_list)
+      vector = -vectors_list(:,i);
+      quiver3(point(1)*grid.resolution, point(2)*grid.resolution, point(3)*grid.resolution, vector(1)*scale, vector(2)*scale, vector(3)*scale, 'r', 'LineWidth', 2, 'MaxHeadSize', 1)
 
+    end
 
+    hold off
 
 
 
