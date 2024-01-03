1dc966ba6aa21c98775d9195a4ee556dcb9f2c41
JB <jakob.baumgartner@hotmail.com>
2024-01-03 17:37:47 +0100

    fixed another kernels bug, added kernel surf visualization, added 3D kernel visualization

--- Code/REP_kernels.m
+++ Code/REP_kernels.m
@@ -1,9 +1,9 @@
-function [kernels] = REP_kernels()
+function [kernels] = REP_kernels(type)
 
     % REP_kernels - Generate directional kernels for a 3D image processing task
     %
     % Usage:
-    %    [kernels] = REP_kernels()
+    %    [kernels] = REP_kernels(type)
     %
     % Description:
     %    This function generates directional kernels for a 3D image processing task.
@@ -15,21 +15,23 @@ function [kernels] = REP_kernels()
     %   directional_kernel_3d(direction, length, sigma1, width, sigma2, height, sigma3, type)
     %
     % Inputs:
-    %    None
+    %    type: 'linear' or 'gaussian'
     %
     % Outputs:
     %    kernels - A cell array containing the generated directional kernels.
 
+    type
+
     % set directional kernels
     sigma = 2
-    kernel_length = 10; % floored to even number
-    kernel_width = 5;
-    kernel_height = kernel_width;
+    kernel_length = 10 % floored to even number
+    kernel_width = 5
+    kernel_height = kernel_width
     
     % form kernels
-    kernel_x = directional_kernel_3d('x', kernel_length, sigma, kernel_width, sigma, kernel_height, sigma, 'gaussian');
-    kernel_y = directional_kernel_3d('y', kernel_length, sigma, kernel_width, sigma, kernel_height, sigma, 'gaussian');
-    kernel_z = directional_kernel_3d('z', kernel_length, sigma, kernel_width, sigma, kernel_height, sigma, 'gaussian');
+    kernel_x = directional_kernel_3d('x', kernel_length, sigma, kernel_width, sigma, kernel_height, sigma, type);
+    kernel_y = directional_kernel_3d('y', kernel_length, sigma, kernel_width, sigma, kernel_height, sigma, type);
+    kernel_z = directional_kernel_3d('z', kernel_length, sigma, kernel_width, sigma, kernel_height, sigma, type);
 
 %     figure()
 %     scatter(1:1:(length(kernel_x(5,:,5))),kernel_x(5,:,5))
--- Code/directional_kernel_3d.m
+++ Code/directional_kernel_3d.m
@@ -12,14 +12,18 @@ function [kernel3D] = directional_kernel_3d(direction, kernel_length, sigma, ker
 
         % extend weights to kernel width
         center_width = floor(kernel_width/2);
-        width_extender = [1:1:center_width center_width flip(1:1:center_width)]/(center_width);
+%         width_extender = [1:1:center_width center_width flip(1:1:center_width)]/(center_width);
+        width_extender = [1:1:center_width flip(1:1:center_width)]/(center_width);
+
 
         % generate 2D kernel
         kernel2D = width_extender' * kernel;
 
         % extend weight to kernel height
         center_height = floor(kernel_height/2);
-        height_extender = [1:1:center_height center_height flip(1:1:center_height)]/(center_height);
+%         height_extender = [1:1:center_height center_height flip(1:1:center_height)]/(center_height);
+        height_extender = [1:1:center_height flip(1:1:center_height)]/(center_height);
+
         
         % create each layer
         kernel3D = repmat(kernel2D, 1, 1, length(height_extender)) .* reshape(height_extender, 1, 1, []);
@@ -36,7 +40,9 @@ function [kernel3D] = directional_kernel_3d(direction, kernel_length, sigma, ker
         % prepare gaussian weights for width
         center_width = floor(kernel_width/2);
         width_extender = exp(-(1:center_width).^2/(2*sigma_width^2)); 
-        width_extender = [flip(width_extender) exp(-(center_width).^2/(2*sigma_width^2)) width_extender];
+%         width_extender = [flip(width_extender) exp(-(center_width).^2/(2*sigma_width^2)) width_extender];
+        width_extender = [flip(width_extender) width_extender];
+
         
         % generate 2D kernel
         kernel2D = width_extender' * kernel;
@@ -44,7 +50,9 @@ function [kernel3D] = directional_kernel_3d(direction, kernel_length, sigma, ker
         % prepare gaussian weights for height
         center_height = floor(kernel_height/2);
         height_extender = exp(-(1:center_height).^2/(2*sigma_height^2));
-        height_extender = [flip(height_extender) exp(-(center_height).^2/(2*sigma_height^2)) height_extender];
+%         height_extender = [flip(height_extender) exp(-(center_height).^2/(2*sigma_height^2)) height_extender];
+        height_extender = [flip(height_extender) height_extender];
+
 
         % create each layer
         kernel3D = repmat(kernel2D, 1, 1, length(height_extender)) .* reshape(height_extender, 1, 1, []);
