2b08567be5cdf0a3fbc582d89319cea86da78344
JB <jakob.baumgartner@hotmail.com>
2024-03-14 22:38:26 +0100

    added labels and titles to the figures

--- Code/figure_vel_dist_join_jvel_vrep_stopexec.m
+++ Code/figure_vel_dist_join_jvel_vrep_stopexec.m
@@ -7,19 +7,23 @@ figure()
 subplot(2,2,1)
 
 % distance
-plot(output.goal_distances, 'DisplayName', 'Position error')
-title('Primary Task')
+plot(output.goal_distances*10, 'DisplayName', 'Position error')
+title('Primary Task', 'Interpreter', 'latex')
 hold on
 
 % orientation
 plot(output.goal_orientation, 'DisplayName', 'Orientation error')
 
 % exec_Stop
-plot(output.exec_slowdown*50, 'DisplayName', 'Primary Task Slowdown')
+plot((output.exec_slowdown)*100, 'DisplayName', 'Primary Task Slowdown')
 hold off
 
 % Add legend
-legend('show')
+legend('show', 'Interpreter', 'latex')
+
+% Add labels
+xlabel('Step', 'Interpreter', 'latex')
+ylabel('Pos. [cm], Orient. [1], Slowd. [%]', 'Interpreter', 'latex')
 
 % Set x-axis limits
 xlim([0 Nmax])
@@ -30,11 +34,15 @@ hold on
 for i = 1:size(output.q_velocities, 1)
     plot(output.q_velocities(i, :)', 'DisplayName', ['$\dot{\theta}_{' num2str(i) '}$'])
 end
-title('Joint Velocities')
+title('$\dot{\theta}$ (Joint Velocities)', 'Interpreter', 'latex')
 hold off
 
 % Add legend
 legend('Interpreter', 'latex', 'Location', 'east')
+
+% Add labels
+xlabel('Step', 'Interpreter', 'latex')
+ylabel('$\dot{\theta}$ [rad/s]', 'Interpreter', 'latex')
 
 % Set x-axis limits
 xlim([0 Nmax])
@@ -45,47 +53,36 @@ hold on
 for i = 1:size(output.joints_positions, 1)
     plot(output.joints_positions(i, :)', 'DisplayName', ['$\theta_{' num2str(i) '}$'])
 end
-title('Joint Positions')
+title('$\theta$ (Joint Positions)', 'Interpreter', 'latex')
 hold off
 
 % Add legend
-legend('Interpreter', 'latex', 'Location', 'east')
+legend('Interpreter', 'latex', 'Location', 'east', 'Interpreter', 'latex')
+
+% Add labels
+xlabel('Step', 'Interpreter', 'latex')
+ylabel('$\theta$ [rad]', 'Interpreter', 'latex')
 
 % Set x-axis limits
 xlim([0 Nmax])
 
 %% Subplot 4: repulsive velocities (v_rep with index)
 subplot(2,2,4)
-% Assuming the cell array is stored in a variable called 'POI_values'
-numTimesteps = 50; % Number of timesteps to process
-numVectors = 7; % Number of velocity vectors per timestep
-
-% Preallocate a matrix to store the norms for efficiency
-velocityNorms = zeros(numTimesteps, numVectors);
-
-for t = 1:numTimesteps
-    % Extract the 3x7 matrix for the current timestep
-    currentMatrix = output.POI_values{t};
-    
-    for v = 1:numVectors
-        % Extract the velocity vector for the current point/object
-        velocityVector = currentMatrix(:, v);
-        
-        % Calculate the norm (magnitude) of the velocity vector
-        velocityNorms(t, v) = norm(velocityVector);
-    end
-end
 
 % Plotting the repulsive velocities norms with index
 hold on
 for v = 1:numVectors
     plot(velocityNorms(:, v), 'DisplayName', ['$v_{rep' num2str(v) '}$'])
 end
-title('Repulsive Velocities (norms)')
+title('$v_{rep}$ Repulsive Velocities', 'Interpreter', 'latex')
 hold off
 
 % Add legend
-legend('Interpreter', 'latex', 'Location', 'east')
+legend('Interpreter', 'latex', 'Location', 'east', 'Interpreter', 'latex')
 
+% Add labels
+xlabel('Step', 'Interpreter', 'latex')
+ylabel('$v_{rep}$ [1]', 'Interpreter', 'latex')
+  
 % Set x-axis limits
 xlim([0 Nmax])
